8025744f1c9cc1da513f4fd52902fc0484cdbb11
Guanqun Mao <gmao@yahoo-inc.com>
2015-09-16 12:36:38 -0700

    Initial Commit

---
+++ chat-client/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  chat-client
+//
+//  Created by Guanqun Mao on 9/16/15.
+//  Copyright (c) 2015 Guanqun Mao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ chat-client/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  chat-client
+//
+//  Created by Guanqun Mao on 9/16/15.
+//  Copyright (c) 2015 Guanqun Mao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
